af300e4c59222a263230d1d7e642bbf9d3f07deb
logicforkidz <info@logicforkidz.com>
2021-05-25 18:09:31 -0700

    Inital commit

---
+++ myDir/myFile_1.py
@@ -0,0 +1,14 @@
+
+
+from myFile_2 import myFunctionTwo
+
+def myFunctionOne ():
+    a = 0
+    a = a + 1
+    print("Function one called and value of A is " + str(a))
+
+myFunctionOne()
+myFunctionTwo()
+
+
+
---
+++ myDir/myFile_2.py
@@ -0,0 +1,6 @@
+
+
+def myFunctionTwo():
+    b = 0
+    b = b + 2
+    print("Function one called and value of B is " + str(b))
